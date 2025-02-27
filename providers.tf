e116f296339fc7f7ac0b602bb1790d1c78d817e0
K1dou <hique1276@gmail.com>
2025-02-26 23:59:37 -0300

    backend

--- providers.tf
+++ providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+ backend "s3" {
+    bucket = "devop-s"
+    key    = "desafio/terraform.tfstate"
+    region = "us-east-1"
+  }
+
+
 }
 
 # Configure the AWS Provider
